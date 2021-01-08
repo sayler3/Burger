8b26a9fc54e5cd6c229046407bb530ba83f05736
Sam Ayler <sayler8701@gmail.com>
2021-01-08 13:56:16 -0800

    adding schema and seeds information

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Salmon burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ bacon burger');
+INSERT INTO burgers (burger_name) VALUES ('Beyond burger');
