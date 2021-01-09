fd769ad3e2b68ec19789c15efbf7036895b3a55c
Sam Ayler <sayler8701@gmail.com>
2021-01-09 09:08:03 -0800

    finish setup for models

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,17 @@
-const orm = require('../config/orm');
+const orm = require("../config/orm");
 
-const burger = {}
+const burger = {
+	selectAll(cb) {
+		orm.selectAll("burgers", (res) => cb(res));
+	},
+
+	insertOne(cols, vals, cb) {
+		orm.insertOne("burgers", cols, vals, (res) => cb(res));
+	},
+
+	updateOne(objColVals, condition, cb) {
+		orm.updateOne("burgers", objColVals, condition, (res) => cb(res));
+	},
+};
 
 module.exports = burger;
